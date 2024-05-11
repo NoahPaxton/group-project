833a836deab369cad0b0307a0078e372099ffac1
NoahPaxton <143596717+noahpaxton@users.noreply.github.com>
2024-05-11 15:17:20 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,9 @@ public class Main {
     static final String GRAPES = "\uD83C\uDF47";
     static final String KIWI = "\uD83E\uDD5D";
     static final String STRAWBERRY = "\uD83C\uDF53";
-
+    //These strings are static final because they do not need to change, and we do not want them to change throughout the project. The value will always remain the same. 
+    //The values of the strings here are all UTF-16 Unicode values for the respective emojis. 
+    
     static final String[] SPINNER_VALUES = new String[]{
             BOMB,
             GREEN_APPLE, RED_APPLE, BANANA, MELON, GRAPES, KIWI, STRAWBERRY,
@@ -38,7 +40,8 @@ public class Main {
             GREEN_APPLE, GREEN_APPLE, GREEN_APPLE, GREEN_APPLE, GREEN_APPLE, GREEN_APPLE,
             GREEN_APPLE, GREEN_APPLE, GREEN_APPLE, GREEN_APPLE, GREEN_APPLE, GREEN_APPLE,
     };
-
+    //This array contains all the values available to the code that accesses this array. 
+    
     public static int option() {
         Scanner input = new Scanner(System.in);
         int number = 0;
@@ -53,7 +56,8 @@ public class Main {
         }
         i = 0;
         return number;
-
+        //Error handling is implemented using a while loop so that it will not 
+        //exit when someone enters something other than 1 or 2. 
     }
 
     public static int menu() {
@@ -62,6 +66,10 @@ public class Main {
         return option;
     }
 
+    //The menu subroutine when called will display then call the option subroutine so an option can be input by the
+    //user.
+  
+    
     public static String[] runSlotGame(int size) {
         String[] lotteryResult = new String[size];
         for (int i = 0; i < size; i++) {
@@ -69,6 +77,7 @@ public class Main {
             lotteryResult[i] = SPINNER_VALUES[randomIndex];
         }
         return lotteryResult;
+        //This subroutine runs the slot function and gives out three random values from the array. 
     }
 
     public static boolean areTheSame(String[] array) {
@@ -78,6 +87,8 @@ public class Main {
             }
         }
         return true;
+        //This subroutine handles the lotteryResult. 
+        //If objects do not equal the same, it will return false and if they do equal the same it will return true. 
     }
 
     public static void main(String[] args) {
@@ -112,6 +123,11 @@ public class Main {
                 System.out.println();
             }
         } while (option == 1);
-        //Quake
+        System.out.println("game has quit");
+        //the main section calls all the subroutine when required. 
+        //It also includes a section where if the resultFruits contains a bomb it will game over.
+        //A while loop is used so that if 1 is selected the main menu will be shown again after being run once.
+        //main will only run while option = 1, if option doesn't equal 1 and is a number, the program will then print
+        //"game has quit" and then the program will exit.
     }
 }
